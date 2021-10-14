9e6cd8cf4d4c95a387f4fca91cdad9340e9e11ff
Roman Bagel <drsg85@gmail.com>
2021-10-14 20:32:03 +0300

    Разделил js код на несколько модулей

---
+++ js/utils/getRandomIntInclusive.js
@@ -0,0 +1,8 @@
+const getRandomIntInclusive = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  if (max > min) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+  throw new Error('Неверные входные данные');
+};
