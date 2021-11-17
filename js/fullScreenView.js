87d00e99aa7835ad07e3ec4f2a6fe75dbaab64a7
Roman Bagel <drsg85@gmail.com>
2021-11-17 19:36:11 +0300

    H/w

--- js/fullScreenView.js
+++ js/fullScreenView.js
@@ -10,6 +10,7 @@ const comments = document.querySelector('.comments-count');
 const socialComment = document.querySelector('.social__comments');
 const picDesc = document.querySelector('.social__caption');
 const closeButton = document.querySelector('.big-picture__cancel');
+// const loadMoreButton = document.querySelector('.comments-loader');
 
 const renderComment = (commentsArr) => {
   const fragment = document.createDocumentFragment();
@@ -37,11 +38,21 @@ const clearSocialComment = () => {
   socialComment.innerHTML = '';
 };
 
+// const test = (commentTest) => {
+//   const testFragment = bigPictureImg.src;
+//   console.log(testFragment);
+// };
+
+// let currectCommentData;
+// const updateCommentData = (dataa) => {
+//   currectCommentData = dataa;
+// };
+
 document.querySelector('.pictures').addEventListener('click', (evt) => {
   const picture = evt.target.closest('.picture');
   if (!evt.target.closest('.picture')) {
-    return
-  };
+    return;
+  }
   const domPictures = document.querySelectorAll('.picture');
   const pictures = Array.from(domPictures);
   const index = pictures.indexOf(evt.target.closest('.picture'));
@@ -52,8 +63,7 @@ document.querySelector('.pictures').addEventListener('click', (evt) => {
   likes.textContent = pictureData.likes;
   picDesc.textContent = pictureData.description;
   comments.textContent = pictureData.comments.length;
-  document.querySelector('.social__comment-count').classList.add('hidden');
-  document.querySelector('.comments-loader').classList.add('hidden');
+  currectCommentData = comments.textContent;
   renderComment(pictureData.comments);
 });
 
